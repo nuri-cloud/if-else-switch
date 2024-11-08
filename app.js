b0d36606db2171300623e43ce61aadb24553931a
nuri-cloud <nurzadatobokelova@gmail.com>
2024-11-08 15:31:09 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+ 
+//  1 тапшырма 
+
+// const seasons = +prompt("What seasons are you looking for");
+// switch (seasons) {
+//     case 12: 
+//         alert("кыш мезгили");
+//         break;
+//     case 1: 
+//         alert("кыш мезгили");
+//         break
+//     case 2: 
+//         alert("кыш мезгили");
+//         break;
+//     case 3:
+//         alert("жаз мезгили");
+//         break;
+//     case  4:
+//         alert("жаз мезгили");
+//         break;
+//     case 5:
+//         alert("жаз мезгили");
+//         break;
+//     case 6:
+//         alert("жай мезгили");
+//         break;
+//     case  7:
+//         alert("жай мезгили");
+//         break;
+//     case  8:
+//         alert("жай мезгили");
+//         break;
+//     case 9:
+//         alert("куз мезгили");
+//         break;
+//     case  10:
+//         alert("куз мезгили");
+//         break;
+//     case  11:
+//         alert("куз мезгили");
+//         break;
+// default:
+//     alert('мындай мезгил жок,  издебейле коюнуз😒')
+
+
+// }
+
+// 2-тапшырма
+
+// const weather = prompt("бугун погода кандай?")
+
+// if (weather <=20){
+//     alert( "кун суук")
+// } else if(weather <=30){
+//     alert("кун жылуу")
+// } else if(weather <= 40){
+//     alert( "кун ыссык")
+// } else if(weather <= 55){
+//     alert("кун аябай ыссык")
+// }else{
+//     alert("error")
+// }
+
+
+//  3-тапшырма
+// const num = prompt( "четный не четный")
+
+// if (num %2===0 ){
+//     alert("четный")
+// } else if (num %3===0){
+//     alert("не четный")
+// }
+
+
+
+
